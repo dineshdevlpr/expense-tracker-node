fca1d169f98046a567017cccbe95f3778a42d9fe
dineshdevlpr <dineshiaf30@gmail.com>
2021-09-22 21:58:30 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+require("dotenv").config();
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+connectDB();
+
+const transactions = require('./routes/transactions');
+
+const app = express();
+
+app.use(express.json());
+
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use('/transactions', transactions);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`.yellow.bold));
+
