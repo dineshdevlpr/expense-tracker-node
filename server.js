5c9b54d57b0b02a4c4248a492674e10e1e6039bf
DINESHKUMAR K <76520994+dineshdevlpr@users.noreply.github.com>
2021-09-23 18:34:11 +0530

    CORS error Solved

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 require("dotenv").config();
+const cors = require("cors");
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
@@ -12,6 +13,7 @@ const transactions = require('./routes/transactions');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
